59ba67b33b266d86b31f46da9af8225aad0de001
coffeerr <chriso1998@qq.com>
2019-02-24 17:15:44 +0800

    crawl one page

---
+++ #001/spider.py
@@ -0,0 +1,11 @@
+#V0.1 基本的爬取
+import requests
+from lxml import etree
+
+# class douban:
+#     #get books in one page
+#     def get_one_page(url):
+#
+#     #get the url of next page
+#     #def get_next_page:
+
